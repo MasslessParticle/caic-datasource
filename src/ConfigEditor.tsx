81ed748a805a2a2d7578d87773ce40fbad9f2c17
Travis Patterson <travis.patterson@grafana.com>
2021-04-14 09:11:02 -0600

    use functional components

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -1,13 +1,9 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { MyDataSourceOptions } from './types';
 
 interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
 
-interface State {}
-
-export class ConfigEditor extends PureComponent<Props, State> {
-  render() {
-    return <div></div>;
-  }
-}
+export const ConfigEditor = (props: Props) => {
+  return <div></div>;
+};
--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,49 +1,46 @@
 import defaults from 'lodash/defaults';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { InlineFormLabel, Select } from '@grafana/ui';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { DataSource } from './datasource';
 import { defaultQuery, MyDataSourceOptions, Region, ZoneQuery } from './types';
 
-const ZONES: Array<SelectableValue<number>> = [
-  { label: 'Entire State', value: Region.EntireState },
-  { label: 'Steamboat & Flat Tops', value: Region.SteamboatFlatTops },
-  { label: 'Front Range', value: Region.FrontRange },
-  { label: 'Vail & Summit County', value: Region.VailSummitCounty },
-  { label: 'Sawatch Range', value: Region.SawatchRange },
-  { label: 'Aspen', value: Region.Aspen },
-  { label: 'Gunnison', value: Region.Gunnison },
-  { label: 'Grand Mesa', value: Region.GrandMesa },
-  { label: 'Northern San Juan', value: Region.NorthernSanJuan },
-  { label: 'Southern San Juan', value: Region.SouthernSanJuan },
-  { label: 'Sangre de Cristo', value: Region.SangreDeCristo },
-];
-
 type Props = QueryEditorProps<DataSource, ZoneQuery, MyDataSourceOptions>;
 
-export class QueryEditor extends PureComponent<Props> {
-  onRegionChange = (value: SelectableValue<number>) => {
-    const { onChange, query, onRunQuery } = this.props;
+export const QueryEditor = (props: Props) => {
+  const zones: Array<SelectableValue<number>> = [
+    { label: 'Entire State', value: Region.EntireState },
+    { label: 'Steamboat & Flat Tops', value: Region.SteamboatFlatTops },
+    { label: 'Front Range', value: Region.FrontRange },
+    { label: 'Vail & Summit County', value: Region.VailSummitCounty },
+    { label: 'Sawatch Range', value: Region.SawatchRange },
+    { label: 'Aspen', value: Region.Aspen },
+    { label: 'Gunnison', value: Region.Gunnison },
+    { label: 'Grand Mesa', value: Region.GrandMesa },
+    { label: 'Northern San Juan', value: Region.NorthernSanJuan },
+    { label: 'Southern San Juan', value: Region.SouthernSanJuan },
+    { label: 'Sangre de Cristo', value: Region.SangreDeCristo },
+  ];
+
+  const onRegionChange = (value: SelectableValue<number>) => {
+    const { onChange, query, onRunQuery } = props;
     onChange({ ...query, zone: value.value });
     onRunQuery();
   };
+  const query = defaults(props.query, defaultQuery);
+  const { zone } = query;
 
-  render() {
-    const query = defaults(this.props.query, defaultQuery);
-    const { zone } = query;
-
-    return (
-      <div className="gf-form">
-        <div className="gf-form-inline">
-          <InlineFormLabel width={12} className="zone-label" tooltip="select a geographic zone">
-            Select a Geographic Zone
-          </InlineFormLabel>
-          <Select width={30} options={ZONES} value={zone} onChange={this.onRegionChange} />
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
+  return (
+    <div className="gf-form">
+      <div className="gf-form-inline">
+        <InlineFormLabel width={12} className="zone-label" tooltip="select a geographic zone">
+          Select a Geographic Zone
+        </InlineFormLabel>
+        <Select width={30} options={zones} value={zone} onChange={onRegionChange} />
+        <div className="gf-form gf-form--grow">
+          <div className="gf-form-label gf-form-label--grow" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
